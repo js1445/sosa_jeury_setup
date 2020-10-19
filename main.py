5c35afc5070d37dd2fa2eeb3b715743315f727a4
js1445 <72766977+js1445@users.noreply.github.com>
2020-10-19 12:50:46 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 def on_forever():
     pass
 forever(on_forever)
+while True:
+    light.show_animation(light.rainbowAnimation, 500)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 forever(function on_forever() {
     
 })
+while (true) {
+    light.showAnimation(light.rainbowAnimation, 500)
+}
